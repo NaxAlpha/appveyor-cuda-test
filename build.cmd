586ef3d4da11d87e92bb3b27bf8ce3470594b39f
Nauman Mustafa <14beenmustafa@seecs.nust.edu.pk>
2017-08-16 10:58:54 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,20 +1,34 @@
 @echo off
-echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_win10-exe -FileName cuda.exe
-echo Installing CUDA toolkit 8
+
+echo "Checking Cache"
+mkdir "cache"
+if NOT EXIST "cache/cuda.exe" (
+    echo "Cache Not Found..."
+    echo "Downloading CUDA toolkit 8"
+    appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_win10-exe -FileName cuda.exe
+    move "cuda.exe" "cache/cuda.exe"
+)
+
+echo "Installing CUDA toolkit 8"
 cuda.exe -s compiler_8.0 ^
             cublas_8.0 ^
             cublas_dev_8.0 ^
             cudart_8.0 ^
             curand_8.0 ^
             curand_dev_8.0
-echo Downloading cuDNN
-appveyor DownloadFile https://www.dropbox.com/s/1kkjlwrpvylemp6/cudnn-8.0-windows10-x64-v7.zip?dl=1 -FileName cudnn.zip
-7z x cudnn.zip -o cudnn
+
+if NOT EXIST "cudnn" (
+    echo "Downloading cuDNN"
+    appveyor DownloadFile https://www.dropbox.com/s/1kkjlwrpvylemp6/cudnn-8.0-windows10-x64-v7.zip?dl=1 -FileName cudnn.zip
+    7z x cudnn.zip -o cudnn
+)
+
+echo "Installation Test:.............."
+
 dir cudnn
 dir cudnn\cuda\bin
 dir cudnn\cuda\lib 
-dir cudnn\cuda\include     
+dir cudnn\cuda\include 
 
 dir .
 
