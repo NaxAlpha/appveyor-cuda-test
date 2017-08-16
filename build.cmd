2f492789a97197f74c8a39c3bf9cf08dec43b7b4
Nauman Mustafa <14beenmustafa@seecs.nust.edu.pk>
2017-08-16 10:47:57 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,16 +1,16 @@
 @echo off
 echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_win10-exe -FileName cuda.exe
 echo Installing CUDA toolkit 8
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
-                           cublas_dev_8.0 ^
-                           cudart_8.0 ^
-                           curand_8.0 ^
-                           curand_dev_8.0
+cuda.exe -s compiler_8.0 ^
+            cublas_8.0 ^
+            cublas_dev_8.0 ^
+            cudart_8.0 ^
+            curand_8.0 ^
+            curand_dev_8.0
 echo Downloading cuDNN
-appveyor DownloadFile https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v5.1/prod/8.0/cudnn-8.0-windows7-x64-v5.1-zip -FileName cudnn-8.0-windows7-x64-v5.1.zip
-7z x cudnn-8.0-windows7-x64-v5.1.zip -ocudnn
+appveyor DownloadFile https://www.dropbox.com/s/1kkjlwrpvylemp6/cudnn-8.0-windows10-x64-v7.zip?dl=1 -FileName cudnn.zip
+7z x cudnn.zip -o cudnn
 dir cudnn
 dir cudnn\cuda\bin
 dir cudnn\cuda\lib 
